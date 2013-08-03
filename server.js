3049c72f329ce7fa0453ae4f186e4a60eb51a449
Christopher Kilding <mr.chris.k@gmail.com>
2013-08-03 18:11:33 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// Barebones static asset server in Node
+// this is because require.js gets irritated when some things are served with file://
+// it prefers everything to be over http://.
+
+// Thanks to http://mahoney.eu/2012/01/04/web-server-on-the-fly-node-js/ for this.
+
+var util = require('util'),
+    connect = require('connect'),
+    port = 3000;
+
+connect.createServer(connect.static(__dirname)).listen(port);
+util.puts('Listening on ' + port + '...');
+util.puts('Press Ctrl + C to stop.');
