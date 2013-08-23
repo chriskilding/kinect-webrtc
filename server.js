5e207c66fcb6d50f2181a5d2008818826415a6e0
Christopher Kilding <mr.chris.k@gmail.com>
2013-08-23 10:06:39 +0100

    package for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 // Thanks to http://mahoney.eu/2012/01/04/web-server-on-the-fly-node-js/ for this.
 var util = require('util'),
     connect = require('connect'),
-    port = 3000;
+    port = process.env.PORT || 3000;
 
 connect.createServer(connect.static(__dirname)).listen(port);
 util.puts('Listening on ' + port + ', Press Ctrl + C to stop...');
